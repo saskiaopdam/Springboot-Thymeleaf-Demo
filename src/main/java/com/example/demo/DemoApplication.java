2078d565139a6dcefbc4876dfb7e05ff7afc7207
Saskia Opdam <opdamsaskia@icloud.com>
2023-07-10 21:36:45 +0200

    change mapping in .java file to write on homepage

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,8 +11,12 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello, %s!", name);
+    // @GetMapping("/hello")
+    // public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    //   return String.format("Hello, %s!", name);
+    // }
+    @GetMapping("/")
+    public String home() {
+      return "Hello, World!";
     }
 }
