0f9bc7a2f10116889b216b115af6d9cb73442ebb
Saskia Opdam <opdamsaskia@icloud.com>
2023-07-12 15:07:01 +0200

    add hello endpoint with name parameter

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,4 +15,8 @@ public class DemoApplication {
     public String home() {
       return "Hello, World!";
     }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
